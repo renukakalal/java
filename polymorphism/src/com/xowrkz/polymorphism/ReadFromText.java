ad0ad4a9c5ae6c1c05c9233555a6ee887e51182c
8088470191 <160418801+8088470191@users.noreply.github.com>
2024-09-12 14:02:05 +0530

    task

---
+++ polymorphism/src/com/xowrkz/polymorphism/ReadFromText.java
@@ -0,0 +1,20 @@
+package com.xowrkz.polymorphism;
+
+public class ReadFromText {
+
+	public static void main(String[] args) {
+		
+		ReadFile fr=new ReadFile(C:\Users\RENUKA\OneDrive\Desktop\basic-coding\SoftwareEnginner.java);
+		BufferReader br=BufferReader(fr);
+		// TODO Auto-generated method stub
+     String str;
+     
+     
+     
+     while(str=br.readLine())!=null)
+     {
+    	 System.out.println(str);
+     }
+	}
+
+}
---
+++ polymorphism/src/com/xworkz/engineering/java/ReadFile.java
@@ -0,0 +1,11 @@
+package com.xworkz.engineering.java;
+
+public class ReadFile {
+	
+	
+	public ReadFile(BufferReader br)
+	{
+	}
+}
+
+
