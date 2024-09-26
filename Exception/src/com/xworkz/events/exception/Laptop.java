412e01450ad9ae394fc6a248bdb29e529d346229
8088470191 <160418801+8088470191@users.noreply.github.com>
2024-09-26 08:25:50 +0530

    checked and unchecked excption

---
+++ Exception/src/com/xworkz/events/exception/Laptop.java
@@ -0,0 +1,15 @@
+package com.xworkz.events.exception;
+
+public class Laptop {
+	
+	public class Loptop extends RuntimeException {
+		
+		public Loptop() 
+		{
+			super();
+			System.out.println("Running no-arg constructor in loptop");
+		}
+
+	}
+
+}
